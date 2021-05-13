c036d271a4139a06d8c85e122e0adc8527ab940e
wangjun3 <wangjun3@msunsoft.com>
2021-05-13 09:14:27 +0800

    add: first

---
+++ src/test/java/com/abbet/scaffold/ScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbet.scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
