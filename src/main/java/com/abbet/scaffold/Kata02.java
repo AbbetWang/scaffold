f7143b2ae46a62bfa4179026b17b58b4716ea508
wangjun3 <wangjun3@msunsoft.com>
2021-05-18 10:04:15 +0800

    update: using recursive methods

--- src/main/java/com/abbet/scaffold/Kata02.java
+++ src/main/java/com/abbet/scaffold/Kata02.java
@@ -2,6 +2,7 @@ package com.abbet.scaffold;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,18 +19,36 @@ public class Kata02 {
     @Deprecated
     public int chop(int value, int[] values) {
         for (int i = 0; i < values.length; i++) {
-            if(value == values[i]){
+            if (value == values[i]) {
                 return i;
             }
         }
         return -1;
     }
-    public int chop(int value, List values){
-        for (int i = 0; i < values.size(); i++) {
-            if(value == (Integer) values.get(i)){
-                return i;
-            }
+
+    public int chop(int value, List values) {
+        if (values.isEmpty()) {
+            return -1;
         }
-        return -1;
+        int low = 0;
+        int high = values.size() - 1;
+        return bitSearch(low, high, value, values);
+    }
+
+    private int bitSearch(int low, int high, int value, List values) {
+        int middle = (low + high) / 2;
+        if (low > high) {
+            return -1;
+        }
+        if (low == high && value != (Integer) values.get(low)) {
+            return -1;
+        }
+        if(value > (Integer)values.get(middle)){
+            return bitSearch(middle+1,high,value,values);
+        }
+        if(value < (Integer)values.get(middle)){
+            return bitSearch(low,middle-1,value,values);
+        }
+        return middle;
     }
 }
